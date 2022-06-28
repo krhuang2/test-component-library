2f05299816a6ff1992ad4655bb1ec00813ddfe4b
Kevin Huang <kevin.huang@genmills.com>
2022-06-27 19:11:09 -0500

    adding styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from './Button.module.scss';
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className={styles.button}>{props.label}</button>;
 };
 
 export default Button;
